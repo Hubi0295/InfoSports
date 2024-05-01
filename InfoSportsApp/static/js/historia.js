08b6212ff701b20a2c1e50944043290b21a6912b
= <nowa nazwa>
2024-05-01 23:26:39 +0200

    3

--- InfoSportsApp/static/js/historia.js
+++ InfoSportsApp/static/js/historia.js
@@ -15,17 +15,22 @@ $(function () {setInterval("slide()", 3000);});
 function toggleDiv() {
             const div = document.getElementById('slideshow');
             if (div.style.display === 'none') {
+                document.getElementById('contentHistoria').innerHTML="";
                 div.style.display = 'block';
             } else {
+                document.getElementById('contentHistoria').innerHTML="";
                 div.style.display = 'none';
             }
 }
 document.addEventListener("DOMContentLoaded", function() {
+
     var but1 = document.getElementById("b1");
     but1.addEventListener('click', function(){
     fetch("http://127.0.0.1:8000/static/historia1950-1960.txt")
     .then( response => {return response.text();} )
-        .then( dane => { document.getElementById("contentHistoria").innerHTML = dane; })
+        .then( dane => {
+        document.getElementById("slideshow").style.display="none";
+        document.getElementById("contentHistoria").innerHTML = dane; })
     },
     false);
 
@@ -33,7 +38,9 @@ document.addEventListener("DOMContentLoaded", function() {
     but2.addEventListener('click', function(){
     fetch("http://127.0.0.1:8000/static/historia1960-1970.txt")
     .then( response => {return response.text();} )
-        .then( dane => { document.getElementById("contentHistoria").innerHTML = dane; })
+        .then( dane => {
+        document.getElementById("slideshow").style.display="none";
+        document.getElementById("contentHistoria").innerHTML = dane; })
     },
     false);
 
@@ -41,39 +48,69 @@ document.addEventListener("DOMContentLoaded", function() {
     but3.addEventListener('click', function(){
     fetch("http://127.0.0.1:8000/static/historia1970-1980.txt")
     .then( response => {return response.text();} )
-        .then( dane => { document.getElementById("contentHistoria").innerHTML = dane; })
+        .then( dane => {
+        document.getElementById("slideshow").style.display="none";
+        document.getElementById("contentHistoria").innerHTML = dane; })
     },
     false);
 
     var but4 = document.getElementById("b4");
-    but3.addEventListener('click', function(){
+    but4.addEventListener('click', function(){
     fetch("http://127.0.0.1:8000/static/historia1980-1990.txt")
     .then( response => {return response.text();} )
-        .then( dane => { document.getElementById("contentHistoria").innerHTML = dane; })
+        .then( dane => {
+        document.getElementById("slideshow").style.display="none";
+        document.getElementById("contentHistoria").innerHTML = dane; })
     },
     false);
 
     var but5 = document.getElementById("b5");
-    but3.addEventListener('click', function(){
-    fetch("http://127.0.0.1:8000/static/historia1990-2000.txt")
+    but5.addEventListener('click', function(){
+    fetch("http://127.0.0.1:8000/static/historia1990-1995.txt")
     .then( response => {return response.text();} )
-        .then( dane => { document.getElementById("contentHistoria").innerHTML = dane; })
+        .then( dane => {
+        document.getElementById("slideshow").style.display="none";
+        document.getElementById("contentHistoria").innerHTML = dane; })
     },
     false);
 
     var but6 = document.getElementById("b6");
-    but3.addEventListener('click', function(){
-    fetch("http://127.0.0.1:8000/static/historia2000-2010.txt")
+    but6.addEventListener('click', function(){
+    fetch("http://127.0.0.1:8000/static/historia1995-2000.txt")
     .then( response => {return response.text();} )
-        .then( dane => { document.getElementById("contentHistoria").innerHTML = dane; })
+        .then( dane => {
+        document.getElementById("slideshow").style.display="none";
+        document.getElementById("contentHistoria").innerHTML = dane; })
     },
     false);
 
     var but7 = document.getElementById("b7");
-    but4.addEventListener('click', function(){
-        fetch("http://127.0.0.1:8000/static/historia2010-2024.txt")
+    but7.addEventListener('click', function(){
+    fetch("http://127.0.0.1:8000/static/historia2000-2007.txt")
+    .then( response => {return response.text();} )
+        .then( dane => {
+        document.getElementById("slideshow").style.display="none";
+        document.getElementById("contentHistoria").innerHTML = dane; })
+    },
+    false);
+
+    var but8 = document.getElementById("b8");
+    but8.addEventListener('click', function(){
+        fetch("http://127.0.0.1:8000/static/historia2007-2010.txt")
         .then( response => {return response.text();} )
-        .then( dane => { document.getElementById("contentHistoria").innerHTML = dane; })
+        .then( dane => {
+        document.getElementById("slideshow").style.display="none";
+        document.getElementById("contentHistoria").innerHTML = dane; })
+        },
+        false);
+
+    var but9 = document.getElementById("b9");
+    but9.addEventListener('click', function(){
+        fetch("http://127.0.0.1:8000/static/historia2010-2012.txt")
+        .then( response => {return response.text();} )
+        .then( dane => {
+        document.getElementById("slideshow").style.display="none";
+        document.getElementById("contentHistoria").innerHTML = dane; })
         },
         false);
     },
