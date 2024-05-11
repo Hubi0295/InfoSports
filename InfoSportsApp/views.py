33a164161a9947ab84918a87860036f3dda8963d
= <nowa nazwa>
2024-05-12 00:31:23 +0200

    4

--- InfoSportsApp/views.py
+++ InfoSportsApp/views.py
@@ -22,11 +22,14 @@ def f1_DaneKierowcy(request):
         tab_naziwsk.append(x["last_name"])
     context["drivers_lastname"] = tab_naziwsk
     parametr = request.GET.get('driverLastName')
+
     if parametr:
         response = urlopen('https://api.openf1.org/v1/drivers?last_name=' + str(parametr) + '&meeting_key=latest&session_key=latest')
         data = json.loads(response.read().decode('utf-8'))[0]
+
         meeting_info = json.loads(urlopen("https://api.openf1.org/v1/meetings?meeting_key="+str(data["meeting_key"])).read().decode('utf-8'))[0]["meeting_name"]
         session_info = json.loads(urlopen("https://api.openf1.org/v1/sessions?session_key="+str(data["session_key"])).read().decode('utf-8'))[0]["session_name"]
+        radio = json.loads(urlopen("https://api.openf1.org/v1/team_radio?meeting_key=latest&driver_number=" + str(data["driver_number"])).read().decode('utf-8'))[0]["recording_url"]
         context["broadcast_name"] = data["broadcast_name"]
         context["country_code"] = data["country_code"]
         context["driver_number"] = data["driver_number"]
@@ -39,6 +42,7 @@ def f1_DaneKierowcy(request):
         context["session_key"] = session_info
         context["team_colour"] = data["team_colour"]
         context["team_name"] = data["team_name"]
+        context["radio"] = radio
         return render(request, "f1_DaneKierowcy.html", context)
     else:
         return render(request, "f1_DaneKierowcy.html", context)
